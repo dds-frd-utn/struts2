afd48abf29410d3e64a3e2ad29fe8f2b2e8bbc92
sergioviera <sergioviera@gmail.com>
2015-09-16 20:19:11 -0300

    initial commit

---
+++ src/utn/frd/action/HelloWorldAction.java
@@ -0,0 +1,25 @@
+package utn.frd.action;
+
+import utn.frd.bean.MessageStore;
+
+import com.opensymphony.xwork2.ActionSupport;
+
+public class HelloWorldAction extends ActionSupport {
+
+	private static final long serialVersionUID = 1L;
+
+	private MessageStore messageStore;
+
+	public String execute() throws Exception {
+		messageStore = new MessageStore();
+		return SUCCESS;
+	}
+
+	public MessageStore getMessageStore() {
+		return messageStore;
+	}
+
+	public void setMessageStore(MessageStore messageStore) {
+		this.messageStore = messageStore;
+	}
+}
---
+++ src/utn/frd/bean/MessageStore.java
@@ -0,0 +1,20 @@
+package utn.frd.bean;
+
+public class MessageStore {
+	private String message;
+
+	public MessageStore() {
+
+		setMessage("Hello Struts User");
+	}
+
+	public String getMessage() {
+
+		return message;
+	}
+
+	public void setMessage(String message) {
+
+		this.message = message;
+	}
+}
